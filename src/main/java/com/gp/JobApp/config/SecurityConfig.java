c93c12e35c4f84c0849da01dcec3ac5cfc986656
gp <gp@mac.mynetworksettings.com>
2025-12-11 15:49:50 -0500

    second commitaat

--- src/main/java/com/gp/JobApp/config/SecurityConfig.java
+++ src/main/java/com/gp/JobApp/config/SecurityConfig.java
@@ -1,73 +1,19 @@
-package com.config;
+package com.gp.JobApp.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-
-    //@Bean
-    //AuthenticationProvider authProvider(){
-    //@Bean
-    //public SecurityFilterChain securityFilterChain(HttpSecurity httpsss) throws Exception {
-
-
-//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf= new Customizer<CsrfConfigurer<HttpSecurity>>() {
-//            @Override
-//            public void customize(CsrfConfigurer<HttpSecurity> httpSecurityCsrfConfigurer) {
-//                httpSecurityCsrfConfigurer.disable();
-//            }
-//        };
-//       http.csrf(customizer->customizer.disable());
-//      http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-      // http.formLogin(Customizer.withDefaults());
-//       http.httpBasic(Customizer.withDefaults());
-//       http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-//        http.csrf(custCsrf);
-
-//        Customizer<AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry<HttpSecurity> custHttp=
-//                new Customizer<AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry<HttpSecurity>>() {
-//            @Override
-//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry
-//                                          registry) {
-//                registry.anyRequest().authenticated();
-//
-//            }
-//        };
-       // http.authorizeHttpRequests(custHttp);
-   //     return http.build();
-    @Bean
-        public AuthenticationProvider authProvider(){
-            DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
-            provider.setUserDetailsService(userDetailsService);
-            provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
-            return provider;
-        }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http
+                .csrf(csrf -> csrf.disable())       // disable CSRF
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // allow all
 
         return http.build();
     }
-
 }
